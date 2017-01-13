d99c57338cf9f178d81470678b511cb6caec318d
Akshay Kadam <akshaykadam0904@gmail.com>
2017-01-13 16:41:55 +0530

    Adding script on Fri Jan 13 16:41:55 IST 2017 :man-kiss-man:

---
+++ SimpleReact/app/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+class Hello extends Component {
+	render() {
+		return (
+				<h1>Hello React</h1>
+			)
+	}
+}
+
+ReactDOM.render(<Hello />, document.getElementById('app'));
